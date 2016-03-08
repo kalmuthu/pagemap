e4ce7650118a61b6a1bc4c3e04368ea42002432a
zvonkok <zvonkok@gmail.com>
2016-03-08 12:15:14 +0100

    Move benchmarks to own repo

--- benchmarks/livermore_loops/block.h
+++
@@ -1,7 +0,0 @@
-/* prototypes of functions defined in block.c
-
-   Copyright (c) 1998.  The Regents of the University of California.
-                    All rights reserved.
-*/
-extern void blockdata(void);
-extern void init_stor(void);
